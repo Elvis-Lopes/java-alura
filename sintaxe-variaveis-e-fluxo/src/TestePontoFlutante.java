717ffb7c20afa0e7c93e160a286d27e2c5a0ed36
Elvis Lopes <elvislopes1996@hotmail.com>
2021-04-03 09:14:45 -0300

    aula sobre casting

---
+++ sintaxe-variaveis-e-fluxo/src/TestePontoFlutante.java
@@ -0,0 +1,12 @@
+
+public class TestePontoFlutante {
+
+	public static void main(String[] args) {
+		 double salario = 1250.70;
+		 double pi = 3.14 / 2;
+		 
+		 System.out.println("meu salario é R$" + salario);
+		 System.out.println("o valor de pi é " + pi);
+	}
+
+}
