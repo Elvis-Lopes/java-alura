25338b3c6b5d39d8b51bb954c1c69a88cb984e64
Elvis Lopes <elvislopes1996@hotmail.com>
2021-04-03 10:24:45 -0300

    inicio da aula sobre String e char

---
+++ sintaxe-variaveis-e-fluxo/src/TesteCaracteres.java
@@ -0,0 +1,16 @@
+
+public class TesteCaracteres {
+
+	public static void main(String[] args) {
+		char letra =  'a';
+		System.out.println(letra);
+		
+		char valor = 65;
+		System.out.println(valor);
+		
+		String palavra = "alura cursos online de tecnologia";
+		System.out.println(palavra);
+		
+	}
+
+}
--- sintaxe-variaveis-e-fluxo/src/TesteConversao.java
+++ sintaxe-variaveis-e-fluxo/src/TesteConversao.java
@@ -6,7 +6,7 @@ public class TesteConversao {
 		int valor = (int) salario;
 		
 		System.out.println(valor);
-
+		
 	}
 
 }
