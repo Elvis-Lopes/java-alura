81417df16593959248c1b5f7389f7939a2c4b3e0
Elvis Lopes <elvislopes1996@hotmail.com>
2021-04-10 12:14:18 -0300

    teste tabuada

---
+++ sintaxe-variaveis-e-fluxo/src/TesteLacoEncadeado.java
@@ -0,0 +1,15 @@
+
+public class TesteLacoEncadeado {
+
+	public static void main(String[] args) {
+		for(int numero =0; numero <= 10;numero++) {
+			for(int multiplicador =0; multiplicador <= 10;multiplicador++) {
+				System.out.print(numero * multiplicador + " ");
+				
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
