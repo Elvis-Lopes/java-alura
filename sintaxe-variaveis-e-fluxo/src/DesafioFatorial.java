6e583878d1264b9d749fc123ce14044e99dd25e0
Elvis Lopes <elvislopes1996@hotmail.com>
2021-04-10 15:23:14 -0300

    Desafio fatorial

---
+++ sintaxe-variaveis-e-fluxo/src/DesafioFatorial.java
@@ -0,0 +1,13 @@
+
+public class DesafioFatorial {
+
+	public static void main(String[] args) {
+		int resultado = 1;
+		for( int fatorial = 1; fatorial <=10; fatorial++) {
+			resultado *= fatorial;
+		}
+		
+		System.out.println(resultado);
+	}
+
+}
