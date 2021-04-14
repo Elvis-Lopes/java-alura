b2860fc28e4119ff8499df9b3e0df3778b10eb2d
Elvis Lopes <elvislopes1996@hotmail.com>
2021-04-13 21:22:05 -0300

    Criado a classe CriaConta e a classe Principal chamada AbrirConta

--- Orientação a Objeto/bytebank/src/bytebank/Conta.java
+++ Orientação a Objeto/bytebank/src/bytebank/Conta.java
@@ -7,6 +7,39 @@ public class Conta {
 	private double saldo;
 	
 	Conta(){
-		
+
 	}
+	
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	
+	public String getTitular() {
+		return titular;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+	
+	public void setNumeroDaConta(int numeroDaConta) {
+		this.numeroDaConta = numeroDaConta;
+	}
+	
+	public int getNumeroDaConta() {
+		return numeroDaConta;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+
 }
---
+++ Orientação a Objeto/bytebank/src/bytebank/CriaConta.java
@@ -0,0 +1,13 @@
+package bytebank;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+	Conta primeiraConta = new Conta();
+	
+	primeiraConta.setSaldo(200);
+	
+	System.out.println(primeiraConta.getSaldo());
+	}
+
+}
