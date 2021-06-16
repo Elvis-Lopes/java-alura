5fb3da0e4a35e5ab0d8620bfb5efec277b2c48ce
Elvis Lopes <elvislopes1996@hotmail.com>
2021-06-15 21:26:29 -0300

    aula apresentando os conceitos da classe Enum

---
+++ enum/Enum/src/Teste.java
@@ -0,0 +1,19 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Thread t = new Thread(() -> System.out.println("Rodando..."));
+		
+		t.setPriority(Thread.MAX_PRIORITY);
+		
+		t.start();
+		
+		Prioridade pMin = Prioridade.MIN;
+		Prioridade pMax = Prioridade.MAX;
+		
+		System.out.println(pMin.name());
+		System.out.println(pMax.ordinal());
+		System.out.println(pMax.getValor());
+	}
+
+}
