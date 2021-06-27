3cffbbc6455c5b4f2e100e274fd2b9f5f13d9c02
Elvis Lopes <elvislopes1996@hotmail.com>
2021-06-26 22:30:55 -0300

    readequação do codigo de try/catch

--- Exceções/java-pilha/src/Fluxo.java
+++ Exceções/java-pilha/src/Fluxo.java
@@ -8,19 +8,20 @@ public class Fluxo {
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+        try {
+        	metodo2();
+        }catch(ArithmeticException e) {
+    		System.out.println("ArithemeticExcepetion");
+    	}
         System.out.println("Fim do metodo1");
     }
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
-        	try {
-        		int a = i/0;
-        	}catch(ArithmeticException e) {
-        		System.out.println("ArithemeticExcepetion");
-        	}
-            System.out.println(i);
+        	System.out.println(i);
+        	int a = i/0;
+            
         }
         System.out.println("Fim do metodo2");
     }
