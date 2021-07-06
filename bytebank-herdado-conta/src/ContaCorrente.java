d37ca49f1ea413e0c946d9c134ce4367346d9337
Elvis Lopes <elvislopes1996@hotmail.com>
2021-07-06 20:45:20 -0300

    correção dos metodos efetuados

--- bytebank-herdado-conta/src/ContaCorrente.java
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -6,13 +6,13 @@ public class ContaCorrente extends Conta implements Tributavel {
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
 	}
-	
+	/*
 	@Override
 	public void saca(double valor) {
 		double valorASacar = valor + 0.2;
 		super.saldo -= valorASacar;
 	}
-
+	*/
 	@Override
 	public void deposita(double valor) {
         super.saldo += valor;
